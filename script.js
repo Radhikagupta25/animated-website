9f1f0e5efea7a1e706d10e84c4a15dd5ffd53589
radhikagupta25 <radhikaaaguptaaa25@gmail.com>
2025-09-20 11:17:18 +0530

    add bg circles

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-let canvas=document.querySelector('canvas');
+let canvas = document.getElementById('canvaaas');
+let c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function Bgcircles(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.draw = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle='white';
+        c.stroke();
+        c.fill();
+    }
+    this.update = function () {
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw();
+    }
+}
+
+let bgcircles = [];
+for (let i = 0; i < 100; i++) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    let dx = (Math.random() - 0.5) * 8;
+    let dy = (Math.random() - 0.5) * 8;
+    let radius = Math.random()*20;
+    bgcircles.push(new Bgcircles(x, y, dx, dy, radius));
+}
+
+function animation() {
+    requestAnimationFrame(animation);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < bgcircles.length; i++) {
+        bgcircles[i].update();
+    }
+}
+animation();
